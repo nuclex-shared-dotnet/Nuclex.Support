313b006f8359024144d3005e17f362943a57f0fc
Markus Ewald <cygon@nuclex.org>
2008-07-17 21:19:41 +0000

    Added license tags in the files where they were amiss; added IntegerHelper class with a method to quickly determine the next power of 2 to any number
    git-svn-id: file:///srv/devel/repo-conversion/nusu@82 d2e56fa2-650e-0410-a79f-9358c0239efd

---
+++ Source/IntegerHelper.Test.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+namespace Nuclex.Support {
+
+  /// <summary>Contains unit tests for the integer helper class</summary>
+  [TestFixture]
+  public class IntegerHelperTest {
+
+    /// <summary>
+    ///   Verifies that the next power of 2 calculation works for long integers
+    /// </summary>
+    [Test]
+    public void TestNextPowerOf2ULong() {
+      Assert.AreEqual(1UL, IntegerHelper.NextPowerOf2(0UL));
+      Assert.AreEqual(1UL, IntegerHelper.NextPowerOf2(1UL));
+      Assert.AreEqual(2UL, IntegerHelper.NextPowerOf2(2UL));
+      Assert.AreEqual(4UL, IntegerHelper.NextPowerOf2(3UL));
+      Assert.AreEqual(4UL, IntegerHelper.NextPowerOf2(4UL));
+      Assert.AreEqual(
+        9223372036854775808UL, IntegerHelper.NextPowerOf2(9223372036854775807UL)
+      );
+      Assert.AreEqual(
+        9223372036854775808UL, IntegerHelper.NextPowerOf2(9223372036854775808UL)
+      );
+    }
+
+    /// <summary>
+    ///   Verifies that the next power of 2 calculation works for long integers
+    /// </summary>
+    [Test]
+    public void TestNextPowerOf2Long() {
+      Assert.AreEqual(1L, IntegerHelper.NextPowerOf2(0L));
+      Assert.AreEqual(1L, IntegerHelper.NextPowerOf2(1L));
+      Assert.AreEqual(2L, IntegerHelper.NextPowerOf2(2L));
+      Assert.AreEqual(4L, IntegerHelper.NextPowerOf2(3L));
+      Assert.AreEqual(4L, IntegerHelper.NextPowerOf2(4L));
+      Assert.AreEqual(4611686018427387904L, IntegerHelper.NextPowerOf2(4611686018427387903L));
+      Assert.AreEqual(4611686018427387904L, IntegerHelper.NextPowerOf2(4611686018427387904L));
+    }
+
+    /// <summary>
+    ///   Verifies that the next power of 2 calculation works for integers
+    /// </summary>
+    [Test]
+    public void TestNextPowerOf2UInt() {
+      Assert.AreEqual(1U, IntegerHelper.NextPowerOf2(0U));
+      Assert.AreEqual(1U, IntegerHelper.NextPowerOf2(1U));
+      Assert.AreEqual(2U, IntegerHelper.NextPowerOf2(2U));
+      Assert.AreEqual(4U, IntegerHelper.NextPowerOf2(3U));
+      Assert.AreEqual(4U, IntegerHelper.NextPowerOf2(4U));
+      Assert.AreEqual(2147483648U, IntegerHelper.NextPowerOf2(2147483647U));
+      Assert.AreEqual(2147483648U, IntegerHelper.NextPowerOf2(2147483648U));
+    }
+
+    /// <summary>
+    ///   Verifies that the next power of 2 calculation works for integers
+    /// </summary>
+    [Test]
+    public void TestNextPowerOf2Int() {
+      Assert.AreEqual(1, IntegerHelper.NextPowerOf2(0));
+      Assert.AreEqual(1, IntegerHelper.NextPowerOf2(1));
+      Assert.AreEqual(2, IntegerHelper.NextPowerOf2(2));
+      Assert.AreEqual(4, IntegerHelper.NextPowerOf2(3));
+      Assert.AreEqual(4, IntegerHelper.NextPowerOf2(4));
+      Assert.AreEqual(1073741824, IntegerHelper.NextPowerOf2(1073741823));
+      Assert.AreEqual(1073741824, IntegerHelper.NextPowerOf2(1073741824));
+    }
+
+
+  }
+
+} // namespace Nuclex.Support
---
+++ Source/IntegerHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Nuclex.Support {
+
+  public static class IntegerHelper {
+
+    /// <summary>Returns the next highest power of 2 from the specified value</summary>
+    /// <param name="value">Value of which to return the next highest power of 2</param>
+    /// <returns>The next highest power of 2 to the value</returns>
+    public static long NextPowerOf2(long value) {
+      return (long)NextPowerOf2((ulong)value);
+    }
+
+    /// <summary>Returns the next highest power of 2 from the specified value</summary>
+    /// <param name="value">Value of which to return the next highest power of 2</param>
+    /// <returns>The next highest power of 2 to the value</returns>
+    public static ulong NextPowerOf2(ulong value) {
+      if(value == 0)
+        return 1;
+
+      --value;
+      value |= value >> 1;
+      value |= value >> 2;
+      value |= value >> 4;
+      value |= value >> 8;
+      value |= value >> 16;
+      value |= value >> 32;
+      ++value;
+
+      return value;
+    }
+
+    /// <summary>Returns the next highest power of 2 from the specified value</summary>
+    /// <param name="value">Value of which to return the next highest power of 2</param>
+    /// <returns>The next highest power of 2 to the value</returns>
+    public static int NextPowerOf2(int value) {
+      return (int)NextPowerOf2((uint)value);
+    }
+
+    /// <summary>Returns the next highest power of 2 from the specified value</summary>
+    /// <param name="value">Value of which to return the next highest power of 2</param>
+    /// <returns>The next highest power of 2 to the value</returns>
+    public static uint NextPowerOf2(uint value) {
+      if(value == 0)
+        return 1;
+
+      --value;
+      value |= value >> 1;
+      value |= value >> 2;
+      value |= value >> 4;
+      value |= value >> 8;
+      value |= value >> 16;
+      ++value;
+
+      return value;
+    }
+
+  }
+
+} // namespace Nuclex.Support
